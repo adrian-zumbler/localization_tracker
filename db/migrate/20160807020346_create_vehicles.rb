8d5b7235c7e21c4abec462c7e016bfab931253ad
Adrian <adrian.meza@peach.solutions>
2016-08-08 09:38:04 -0500

    successfully migrate for vehicle model

--- db/migrate/20160807020346_create_vehicles.rb
+++ db/migrate/20160807020346_create_vehicles.rb
@@ -1,6 +1,10 @@
 class CreateVehicles < ActiveRecord::Migration
   def change
     create_table :vehicles do |t|
+      t.string :code, null:false
+      t.string :serial_numnber, null:false
+      t.decimal :last_lng, {precision:10,scale:6}
+      t.decimal :last_lat,{precision:10,scale:6}
 
       t.timestamps null: false
     end

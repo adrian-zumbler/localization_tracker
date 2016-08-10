eec0a78eacf551c9442c7fc3428837396e29d5f8
Adrian <adrian.meza@peach.solutions>
2016-08-10 12:02:03 -0500

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,3 @@
 
 vehicleX = Vehicle.create(code:"0001",serial_numnber:"100002")
 vehicleY = Vehicle.create(code:"0002",serial_numnber:"100003")
-
-VehicleLocalization.create([
-  {vehicle:vehicleX,lat:17.953959,lng:-102.175619},
-  {vehicle:vehicleX,lat:17.954000,lng:-102.177000},
-  {vehicle:vehicleX,lat:17.958000,lng:-102.178000},
-  {vehicle:vehicleX,lat:17.951000,lng:-102.179000},
-  {vehicle:vehicleX,lat:17.960000,lng:-102.174000}
-  ])

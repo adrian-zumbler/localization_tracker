b572bc810170a2088053070545aae25c1b684d04
Adrian <adrian.meza@peach.solutions>
2016-08-10 12:06:45 -0500

    added function to update the last_postition for vehicle

--- app/models/vehicle_localization.rb
+++ app/models/vehicle_localization.rb
@@ -2,4 +2,14 @@ class VehicleLocalization < ActiveRecord::Base
   default_scope { order('created_at ASC') }
 
   belongs_to :vehicle
+
+  before_create :update_last_position
+
+  private
+
+  def update_last_position
+    self.vehicle.last_lat = lat
+    self.vehicle.last_lng = lng
+    self.vehicle.save
+  end
 end
